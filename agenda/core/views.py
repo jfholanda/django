f1e222aaa4bd1ae8931cc9dfbd17900798683187
jfholanda <joaofelipeholanda@gmail.com>
2023-09-04 23:45:41 -0300

    html templates and more

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+
+# def index(request):
+#     return redirect('/agenda/')
+
+def lista_eventos(request):
+    eventos = Evento.objects.all()
+    response = {'eventos': eventos}
+
+    return render(request, 'agenda.html', response)
